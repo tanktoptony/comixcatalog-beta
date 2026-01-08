ff2fc09c2a1c0d376fed6abfc99f2ebd6c2d9a2d
Anthony Jarina <anthony.jarina@gmail.com>
2026-01-08 16:51:55 -0600

    loading added to header issues

--- src/components/Header.js
+++ src/components/Header.js
@@ -59,7 +59,7 @@ export default function Header() {
           </Link>
 
           {/* Only beta users who are NOT logged in */}
-          {hasAccess && !user &&(
+          {hasAccess && !loading &&!user &&(
             <>
               <Link href="/login" className="nav-link">
                 Login
